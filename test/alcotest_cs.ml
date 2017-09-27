0c958e81005982f692caed25017ba6e912345cf1
Your Name <you@example.com>
2017-09-27 20:51:24 +0200

    more TAI64 testing

--- test/alcotest_cs.ml
+++ test/alcotest_cs.ml
@@ -1,6 +1,6 @@
 let cs = Alcotest.testable Cstruct.hexdump_pp Cstruct.equal
 
-let ptime = Alcotest.testable (Ptime.pp_rfc3339 ())
+let ptime = Alcotest.testable (Ptime.pp_rfc3339 ~frac_s:1000000())
                               (fun a b -> 0 = Ptime.compare a b)
 
 let get_opt = function Some x -> x | None -> failwith "get_opt"
@@ -53,16 +53,31 @@ let test_strip_leading_char () =
 
 let test_tai64 () =
   let test_helper hex second =
-    let mt = Cs.e_ptime_of_tai64 `e (Cs.of_hex hex |> R.get_ok) |> R.get_ok in
-    let ct = Cs.create_tai64_of_ptime mt in
-    let st = get_opt (Ptime.Span.of_int_s second |> Ptime.of_span) in
-    Alcotest.(check ptime)  hex st mt ;
-    Alcotest.(check string) hex (Cs.to_hex ct) (String.lowercase_ascii hex) ;
-    Alcotest.(check cs)     hex ct (Cs.create_tai64_of_ptime st)
+    let unhexed = Cs.of_hex hex |> R.get_ok in
+    let t,tn = Cs.e_ptime_of_tai64 `e unhexed |> R.get_ok,
+               Cs.e_ptime_of_tai64_n `e unhexed |> R.get_ok
+    in
+    let ct,ctn = Cs.create_tai64_of_ptime t,
+                 Cs.create_tai64_n_of_ptime tn
+    in
+    let st = match second with
+    | `D x -> (match Ptime.of_rfc3339 x |> R.get_ok with t,_,_->t)
+    | `S s -> get_opt (Ptime.Span.of_int_s s |> Ptime.of_span) in
+    Alcotest.(check ptime)  ("st = tn: "^hex) st tn ;
+    Alcotest.(check string) ("str: "^hex)
+      (Cs.to_hex ctn) (String.lowercase_ascii hex) ;
+    Alcotest.(check cs)  ("ct=stn: "^hex) ct (Cs.create_tai64_of_ptime st) ;
+    Alcotest.(check cs)  ("ctn=stn: "^hex) ctn (Cs.create_tai64_n_of_ptime st)
   in
-  (* From http://cr.yp.to/libtai/tai64.html *)
-  test_helper "3fFFffFFffFFffFF" (-1) ;
-  test_helper "400000002a2b2c2d" 707472429
+  (* From http://cr.yp.to/libtai/tai64.html
+   * NOTE that these currently fail because leap seconds are not implemented *)
+  test_helper "3fFFffFFffFFffFF00000000" @@ `S (-1) ;
+  test_helper "400000002a2b2c2d00000000" @@ `S 707472429 ;
+  test_helper "3fFFffFFa1f2cd8a00000000" @@ `S (-1577923200) ;
+  test_helper "3fFFffFF8000000000000000" @@ `S (-2147483648) ;
+  test_helper "400000007fFFffFF00000000" @@ `S 2147483647 ;
+  test_helper "400000000000000000000000" @@ `S 0 ;
+  test_helper "4000000055932da2362888d3" @@ `D "2015-06-30T23:59:59.908626131Z"
 
 let tests =
   [ "Cs.to_list", `Quick, test_to_list
